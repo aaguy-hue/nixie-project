8efe5447dbdd454118b43739dd240e564de46422
aaguy-hue <aaditmathur6@gmail.com>
2026-07-23 14:20:48 -0700

    simple nixie control

--- software/nixie_simple/nixie_simple.ino
+++ software/nixie_simple/nixie_simple.ino
@@ -1,40 +1,26 @@
-int C = 9;
+int C = 8;
 int B = 10;
 int D = 11;
 int A = 12;
 
-int digits [10][4] {
-  {0,0,0,0},
-  {0,0,0,1},
-  {0,0,1,0},
-  {0,0,1,1},
-  {0,1,0,0},
-  {0,1,0,1},
-  {0,1,1,0},
-  {0,1,1,1},
-  {1,0,0,0},
-  {1,0,0,1}
-};
+int num = 0;
 
 void setup() {
-  // setup all the pins
   pinMode(C, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(D, OUTPUT);
   pinMode(A, OUTPUT);
+}
 
-  digitalWrite(D, LOW);
-  digitalWrite(C, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(A, LOW);
+void writeNumber(char num) {
+  digitalWrite(D, (num >> 3)&1);
+  digitalWrite(C, (num >> 2)&1);
+  digitalWrite(B, (num >> 1)&1);
+  digitalWrite(A, (num     )&1);
 }
 
 void loop() {
-  // for(int i=0; i<10; i++) {
-  //   digitalWrite(OUT_PIN_A,digits[i][3]);
-  //   digitalWrite(OUT_PIN_B,digits[i][2]);
-  //   digitalWrite(OUT_PIN_C,digits[i][1]);
-  //   digitalWrite(OUT_PIN_D,digits[i][0]);
-  //   delay(1000);
-  // }
+  writeNumber(num);
+  num = (num+1)%10;
+  delay(50);
 }
