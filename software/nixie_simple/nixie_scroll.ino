57445e0733a14a5d328d7cbdc06693e7417c23d0
aaguy-hue <aaditmathur6@gmail.com>
2026-07-29 23:18:03 -0700

    forgot to commit this file

--- software/nixie_simple/nixie_scroll.ino
+++ software/nixie_simple/nixie_scroll.ino
@@ -3,15 +3,18 @@ int B = 10;
 int D = 11;
 int A = 12;
 
-int num = 0;
+char num = 0;
 
 void setup() {
   pinMode(C, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(D, OUTPUT);
   pinMode(A, OUTPUT);
+  writeNumber(num);
 }
 
+// Write a number to the nixie tube
+// Digits above 9 will turn off the nixie tube bc of how the SN74141/K155ID1 works
 void writeNumber(char num) {
   digitalWrite(D, (num >> 3)&1);
   digitalWrite(C, (num >> 2)&1);
@@ -20,7 +23,7 @@ void writeNumber(char num) {
 }
 
 void loop() {
-  writeNumber(num);
   num = (num+1)%10;
+  writeNumber(num);
   delay(50);
 }
